97900de4d547f015be6825f49c5a47fa68548313
R Jay <15009097+J4CODE@users.noreply.github.com>
2019-06-23 21:13:11 -0400

    Added a task function (Add Task)

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ function loadEventListeners() {
   // Filter tasks event
   filter.addEventListener("keyup", filterTasks);
 }
+
 // Get Tasks from LS
 function getTasks() {
   let tasks;
@@ -59,4 +60,43 @@ function getTasks() {
     // Append li to ul
     taskList.appendChild(li);
   });
+}
+
+// Add a task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  // Create an li element
+  const li = document.createElement("li");
+
+  // Add a class
+  li.className = "collection-item";
+
+  // Create a text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  //Create a new link element
+  const link = document.createElement("a");
+
+  // Add a class
+  link.className = "delete-item secondary-content";
+
+  // Add an icon HTML
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Store in LS
+  storeTaskInLocalStorage(taskInput.value);
+
+  // Clear Input
+  taskInput.value = "";
+
+  e.preventDefault();
 }
