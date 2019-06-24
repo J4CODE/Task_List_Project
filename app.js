d32a75769457244a9951501aa763bd9a2d7afa9d
R Jay <15009097+J4CODE@users.noreply.github.com>
2019-06-23 21:09:12 -0400

    Created all the event JS listeners

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ const taskList  = document.querySelector(".collection");
 const clearBtn  = document.querySelector(".clear-tasks");
 const filter    = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
+
+// Load All The Event Listeners
+loadEventListeners();
+
+// Load All The Event Listeners
+function loadEventListeners() {
+  // DOM Load event
+  document.addEventListener("DOMContentLoaded", getTasks);
+
+  // Add task event
+  form.addEventListener("submit", addTask);
+
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+
+  // Clear task event
+  clearBtn.addEventListener("click", clearTasks);
+
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
+}
