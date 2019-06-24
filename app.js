8dfe295448118e4fe8ac9057d02f32b1efe1efc1
R Jay <15009097+J4CODE@users.noreply.github.com>
2019-06-23 21:11:02 -0400

    Created getting the tasks from list function

--- app.js
+++ app.js
@@ -25,3 +25,38 @@ function loadEventListeners() {
   // Filter tasks event
   filter.addEventListener("keyup", filterTasks);
 }
+// Get Tasks from LS
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function(task) {
+    // Create an li element
+    const li = document.createElement("li");
+
+    // Add a class
+    li.className = "collection-item";
+
+    // Create a text node and append to li
+    li.appendChild(document.createTextNode(task));
+
+    //Create a new link element
+    const link = document.createElement("a");
+
+    // Add a class
+    link.className = "delete-item secondary-content";
+
+    // Add an icon HTML
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+  });
+}
