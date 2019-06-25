621b927a3ba4370f0232ab50e551b0ace0fafa70
R Jay <15009097+J4CODE@users.noreply.github.com>
2019-06-25 00:10:20 -0400

    Added the Filter Tasks function

--- app.js
+++ app.js
@@ -166,3 +166,17 @@ function clearTasks() {
 function clearTasksFromLocalStorage() {
   localStorage.clear();
 }
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
+}
